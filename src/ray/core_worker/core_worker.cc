606cccc7a3c997046560573143601dfe7bba740a
Alexey Kudinkin <ak@anyscale.com>
2023-12-22 19:14:18 -0800

    Cleaned up all refs
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -142,8 +142,7 @@ CoreWorker::CoreWorker(const CoreWorkerOptions &options, const WorkerID &worker_
                                   std::placeholders::_4,
                                   std::placeholders::_5,
                                   std::placeholders::_6,
-                                  std::placeholders::_7,
-                                  std::placeholders::_8);
+                                  std::placeholders::_7);
     direct_task_receiver_ = std::make_unique<CoreWorkerDirectTaskReceiver>(
         worker_context_, task_execution_service_, execute_task, [this] {
           return local_raylet_client_->TaskDone();
@@ -2610,7 +2609,6 @@ Status CoreWorker::ExecuteTask(
     const std::shared_ptr<ResourceMappingType> &resource_ids,
     std::vector<std::pair<ObjectID, std::shared_ptr<RayObject>>> *return_objects,
     std::vector<std::pair<ObjectID, std::shared_ptr<RayObject>>> *dynamic_return_objects,
-    std::vector<std::pair<ObjectID, bool>> *streaming_generator_returns,
     ReferenceCounter::ReferenceTableProto *borrowed_refs,
     bool *is_retryable_error,
     std::string *application_error) {
@@ -2742,7 +2740,6 @@ Status CoreWorker::ExecuteTask(
       task_spec.GetSerializedRetryExceptionAllowlist(),
       return_objects,
       dynamic_return_objects,
-      streaming_generator_returns,
       creation_task_exception_pb_bytes,
       is_retryable_error,
       application_error,
@@ -2941,7 +2938,6 @@ Status CoreWorker::ReportGeneratorItemReturns(
     const rpc::Address &caller_address,
     int64_t item_index,
     uint64_t attempt_number,
-    std::vector<std::pair<ObjectID, bool>> *streaming_generator_returns,
     std::shared_ptr<GeneratorBackpressureWaiter> waiter
 ) {
   rpc::ReportGeneratorItemReturnsRequest request;
@@ -3074,12 +3070,10 @@ std::vector<rpc::ObjectReference> CoreWorker::ExecuteTaskLocalMode(
   std::string application_error = "";
   // TODO(swang): Support DynamicObjectRefGenerators in local mode?
   std::vector<std::pair<ObjectID, std::shared_ptr<RayObject>>> dynamic_return_objects;
-  std::vector<std::pair<ObjectID, bool>> streaming_generator_returns;
   RAY_UNUSED(ExecuteTask(task_spec,
                          resource_ids,
                          &return_objects,
                          &dynamic_return_objects,
-                         &streaming_generator_returns,
                          &borrowed_refs,
                          &is_retryable_error,
                          &application_error));
